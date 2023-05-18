bc155bc8dddbd84942759ebe3d966a5a888a6df2
Khushahal Sharma <113673132+Khushahal007@users.noreply.github.com>
2023-05-18 11:53:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
-const express=require('express')
-const db=require('./db')
-const userModel=require('./Model/userModel')
-const userRoute=require('./Routes/userRoute')
-const cors=require("cors")
-const dotenv=require("dotenv")
+const express = require('express')
+const db = require('./db')
+const userModel = require('./Model/userModel')
+const userRoute = require('./Routes/userRoute')
+const cors = require("cors")
+const dotenv = require("dotenv")
 dotenv.config();
 
-const app=express()
+const app = express()
 
 
-const port=process.env.PORT;
+const port = process.env.PORT;
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
 app.use('/api', userRoute)
 
 app.get("/", (req, res) => {
     console.log("Server is running...")
 })
 
-app.listen(port, ()=>{
-    console.log("Server is running on "+port)
+app.listen(port, () => {
+    console.log("Server is running on " + port)
 })
